46c88eff2f88762fc5fd595c09c3ff34d6b3e6d7
amrossx <ambrosinorossella@gmail.com>
2021-08-18 15:11:43 +0200

    aggiornato get deco

--- get_decodifica.php
+++ get_decodifica.php
@@ -8,13 +8,18 @@ if(!$conn) {
     die('Connessione fallita !<br />');
 } else {
 	//$idcivico=$_GET["id"];
-	$tabella= $_GET["t"];
-	$campo= $_GET["c"];
+	$tabella = pg_escape_string($_GET["t"]);
+	$tabella_ok=str_replace('t_','',$tabella);
+	$campo = pg_escape_string($_GET["c"]);
 	$campo_ok=str_replace('d_','',$campo);
+	$campo_ok=str_replace('fk_','',$campo_ok);
+	if ($tabella =='' or $campo ==''){
+		die('[{"ERROR":"Il WS richiede come parametri di input il nome della tabella (t) e il nome del campo da decodificare (c). Verifica di averli correttamente inseriti"}]');
+	}
 	if ($campo == 'd_lato'){
 		$query="SELECT * From normativa.lato;";
 	} else {
-		$query="SELECT * From normativa.".$tabella."_".$campo_ok.";";
+		$query="SELECT * From normativa.".$tabella_ok."_".$campo_ok.";";
     }
     //echo $query;
 	$result = pg_query($conn, $query);
@@ -30,7 +35,7 @@ if(!$conn) {
 		//print $rows;
 		print json_encode(array_values(pg_fetch_all($result)));
 	} else {
-		echo "[{\"NOTE\":'No data'}]";
+		echo '[{"NOTE":"No data"}]';
 	}
 }
 
@@ -39,6 +44,3 @@ if(!$conn) {
 
 
 ?>
-
-
-
