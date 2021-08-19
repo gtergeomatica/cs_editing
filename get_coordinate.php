18cfe82c579ed6462c4379eb1f7d0b0ff3f39bd1
amrossx <ambrosinorossella@gmail.com>
2021-08-19 10:06:02 +0200

    checkout from master

---
+++ get_coordinate.php
@@ -0,0 +1,50 @@
+<?php 
+
+session_start();
+
+require('./conn.php');
+
+/*Dati una progressiva e un codice strada 
+  il servizio get_coordinate restituisce le coordinate geografiche e il codice strada*/ 
+
+if(!$conn) {
+    die('Connessione fallita !<br />');
+} else {
+	//$idcivico=$_GET["id"];
+	$cod_strada = pg_escape_string($_GET["cod_strada"]);
+	$prog = pg_escape_string($_GET["prog"]);
+	if ($cod_strada =='' OR $prog =='') {
+		die('[{"ERROR":"Il WS richiede come input sia il codice strada che la progressiva"}]');
+	} else {
+		$query= 'SELECT st_x(st_transform(geom,4326)) as lon , st_y(st_transform(geom,4326)) as lat, cod_strada FROM
+		(SELECT ST_Force3D(ST_LineInterpolatePoint(ST_LineMerge(e.geom),
+		(('.$prog.'-e.prog_ini)/(e.prog_fin-e.prog_ini)*st_length(e.geom))
+		/st_length(e.geom))) as geom , cod_strada
+		FROM geometrie.elementi_stradali e 
+		WHERE e.cod_strada = \''.$cod_strada.'\'
+		and  e.prog_ini < '.$prog.' 
+		and e.prog_fin > '.$prog.') as foo;';
+		$result = pg_query($conn, $query);
+		//echo $query;
+		#exit;
+		$rows = array();
+		while($r = pg_fetch_assoc($result)) {
+				$rows[] = $r;
+				//echo $r['progressiva'];
+		}
+		//pg_close($conn);
+		#echo $rows ;
+		if (empty($rows)==FALSE){
+			//print $rows;
+			print json_encode(array_values(pg_fetch_all($result)));
+		} else {
+			echo '[{"NOTE":"No data"}]';
+		}
+		pg_close($conn);
+
+
+
+	}
+}
+
+?>
---
+++ get_route_by_cod.php
@@ -0,0 +1,111 @@
+<?php 
+
+session_start();
+
+
+$cod=pg_escape_string($_GET['cod']);  
+
+require('./conn.php');
+/*Dato un codice strada restituisce 
+nome del layer,  crs, denominazione_ufficiale, 
+prog_ini, prog_fin, descrizione, note, geometria*/ 
+
+if(!$conn) {
+    die('Connessione fallita !<br />');
+} else {
+    $query="SELECT cod_strada, denominazione_ufficiale, 
+    prog_ini, prog_fin, descrizione, note, 
+    st_asgeojson(st_curvetoline(st_transform(geom, 4326))) as geometry
+    FROM geometrie.route 
+    WHERE cod_strada = $1;";
+    
+    //WHERE cod_strada = '". $cod ."';";
+    
+    //echo $query;
+    //$result = pg_query($conn, $query);
+    $result=pg_prepare($conn, "myq", $query);
+    $result=pg_execute($conn, "myq", array($cod));
+    //echo $query;
+    //exit;
+    $rows = array();
+    
+    $geojson ='{
+        "type": "FeatureCollection",
+        "name": "route",
+        "crs": {
+        "type": "name",
+        "properties": {
+            "name": "EPSG:4326",
+        }
+        },
+        "features":[';
+    $k=1;
+    while($r = pg_fetch_assoc($result)) {
+        // popolo le Feature del geojson
+        $row2 = array();
+        $array2 = array (
+            "cod_strada" => $r["cod_strada"],
+            "denominazione_ufficiale" => $r["denominazione_ufficiale"], 
+            "prog_ini" => $r["prog_ini"], 
+            "prog_fin" => $r["prog_fin"], 
+            "descrizione" => $r["descrizione"],
+            "note"  => $r["note"]
+        );
+        $row2[] = $array2;
+        $geom=str_replace("[{", "{",
+            str_replace("}]","}",str_replace("\\", "",$r['geometry'])));
+        if ($k==1){
+            $geojson= $geojson.'{"type":"Feature","geometry":'.$geom;
+        } else {
+            $geojson= $geojson.',{"type":"Feature","geometry":'.$geom;
+        }
+        $geojson= $geojson. ',"properties":'.str_replace("[{", "{",
+            str_replace("}]","}",json_encode(array_values($row2)).'}'));
+        /*$array1 = array (
+            "type" => 'Feature',
+            "geometry" => $r["geometry"],
+            "properties" => json_encode(array_values($row2))
+        );
+        $rows[] = str_replace('"properties":"', '"properties":', $array1);*/
+        $rows=$array2;
+        $k=$k+1;
+    }
+    
+    pg_close($conn);
+    #echo $rows ;
+    if (empty($rows)==FALSE){
+        //print $rows;
+        #print json_encode(array_values(pg_fetch_all($result)));
+        /*{
+            "type": "FeatureCollection",
+            "name": "b7",
+            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+            "features": [*/
+        /*$geojson ='{
+            "type": "FeatureCollection",
+            "name": "route",
+            "crs": {
+            "type": "name",
+            "properties": {
+                "name": "EPSG:4326",
+            },
+            },
+            "features":['.str_replace("[{", "{",
+            str_replace("}]","}", 
+                str_replace('"{', '{',
+                    str_replace('}"', '}',
+                        str_replace("\\", "", json_encode(array_values($rows))))))).']}';*/
+        
+    
+        print $geojson.']}';
+    } else {
+        echo "[{\"NOTE\":'No data'}]";
+    }
+    
+}
+
+
+
+
+
+?>
